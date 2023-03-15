e48c7c82f29280be4948f37573f17707ebb468dc
Isaac Oluwatemilorun <akiolisa@gmail.com>
2023-03-15 10:46:30 +0100

    Add Montserrat font as default sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        black: "rgba(18, 18, 18, 1)",
+      },
+    },
   },
   plugins: [],
-}
+};
